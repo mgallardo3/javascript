26f499b66e81f1d8d934878f932dbed9911c26ec
mgallardo3 <mgallardo3@mail.greenriver.edu>
2019-04-10 13:24:43 -0400

    Created a directory for js2 exercises

---
+++ js2/heehaw.js
@@ -0,0 +1,14 @@
+/*
+Maria Gallardo
+04/10/2019
+heehaw.js
+This file prints strings to the console, depending of the type of number
+*/
+
+for (let numb =0; numb <=100; num+1)
+{
+    if((num % 3 ==0) || (num % 5 == 0))
+    {
+        console.log("haa");
+    }
+}
