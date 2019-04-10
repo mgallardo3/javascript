8e082d249f8674e84d412ffa3a427fdc8eda9fdf
mgallardo3 <mgallardo3@mail.greenriver.edu>
2019-04-10 14:12:23 -0400

    Added a javascript file with a function that prints hee Haw depending on the number

--- js2/heehaw.js
+++ js2/heehaw.js
@@ -5,10 +5,20 @@ heehaw.js
 This file prints strings to the console, depending of the type of number
 */
 
-for (let numb =0; numb <=100; num+1)
+for (let num =1; num <=100; num+=1)
 {
-    if((num % 3 ==0) || (num % 5 == 0))
+    if((num % 3 === 0) && (num %5 ===0))
     {
-        console.log("haa");
+        console.log("Hee Haw! ");
+    }
+
+    else if(num % 3 === 0)
+    {
+        console.log("Hee! ");
+    }
+
+    else if(num %5 ===0)
+    {
+        console.log("Haw! ");
     }
 }
