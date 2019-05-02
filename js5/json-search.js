0d5aae7f9f200cf34d3dc4c2c9c97336b553d727
mgallardo3 <mgallardo3@mail.greenriver.edu>
2019-05-01 23:33:51 -0400

    Add a html file with a title and a script to json-search.js

---
+++ js5/json-search.js
@@ -0,0 +1,54 @@
+/*
+Maria Gallardo
+05/01/2019
+json-search.js
+
+This file contains a set of objects to and methods to display those objects in
+json-search.html file
+*/
+
+//Create Json file with at least 5 people name, sex, born, died, father, and mother.
+var people = [
+
+    {
+        name: "Jane Doe",
+        sex : "female",
+        born: 1955,
+        died: 1999,
+        father: "Todd Doe",
+        mother: "Angie Doe"
+    },
+    {
+        name: "Jane Ramirez",
+        sex : "female",
+        born : 1935,
+        died : 1997,
+        father : "Tom Ramirez",
+        mother : "Lina Ramirez"
+    },
+    {
+        name : "Joe Smith",
+        sex : "male",
+        born : 1985,
+        died : 2005,
+        father : "Calvin Smith",
+        mother : "Mary Smith"
+    },
+    {
+        name: "Noah Miller",
+        sex : "male",
+        born : 1901,
+        died : 2001,
+        father : "James Miller",
+        mother : "Ava Miller"
+    },
+    {
+        name : "Jacob Brown",
+        sex : "male",
+        born : 1934,
+        died : 1979,
+        father : "Mason Brown",
+        mother : "Bella Brown"
+    }
+
+];
